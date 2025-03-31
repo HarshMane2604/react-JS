af2e0599ecddfd4a4d63c63fcb29e33be08936a5
HarshMane2604 <maneharsh72@gmail.com>
2025-03-31 17:34:11 +0530

    Task manager web Application using react

---
+++ 01TaskManager/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="text-4xl text-white font-bold">
+      <h1 className='p-4 bg-transparent shadow-lg shadow-emerald-500/50'>Task Manager Application using React</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
